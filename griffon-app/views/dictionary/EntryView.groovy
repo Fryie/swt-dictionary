48ec2e3b8603f03135d09703a0fba689b8d1610f
Pierpaolo Frasa <pfrasa@smail.uni-koeln.de>
2012-08-27 22:04:46 +0200

    Improved appearance somewhat

--- griffon-app/views/dictionary/EntryView.groovy
+++ griffon-app/views/dictionary/EntryView.groovy
@@ -2,27 +2,27 @@ package dictionary
 
 cTabItem(tabGroup,text:"  "+model.name+"  ",id:"tab") {
 	composite {
-		migLayout(layoutConstraints:"wrap 2")
+		migLayout(layoutConstraints:"wrap 2",columnConstraints:"[]rel[grow]")
 		label(text:"Name:")
-		text(text:model.name) {
+		text(text:model.name,layoutData:"growx") {
 			onEvent(type:'Modify') { evt -> 
 				model.name=evt.widget.text
 			 }
 		}
 		label(text:"Translation:")
-		text(text:model.translation) {
+		text(text:model.translation,layoutData:"growx") {
 			onEvent(type:'Modify') { evt -> 
 				model.translation=evt.widget.text
 			 }
 		}
 		label(text:"Grammatical categories:")
-		text(text:model.categories) {
+		text(text:model.categories,layoutData:"growx") {
 			onEvent(type:'Modify') { evt -> 
 				model.categories=evt.widget.text
 			 }
 		}
-		label(text:"Notes:")
-		text(text:model.notes) {
+		label(text:"Notes:",layoutData:"top, left")
+		text(text:model.notes,style:"multi, border, wrap, v_scroll",layoutData:"growx, h 200") {
 			onEvent(type:'Modify') { evt -> 
 				model.notes=evt.widget.text
 			 }
